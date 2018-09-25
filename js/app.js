b2217ed146726d1a6410140f4dd0f77ef75007d7
SoullessVoid <39170422+SoullessVoid@users.noreply.github.com>
2018-09-24 17:31:29 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,9 @@
-// Scroll to specific values
-// scrollTo is the same
-window.scroll({
-    top: 2500,
-    left: 0,
-    behavior: 'smooth'
-});
+$("#download").click(function() {
+    $(".modal").addClass("is-active");
+    setTimeout(function() {},5000);
+})
 
-// Scroll certain amounts from current position 
-window.scrollBy({
-    top: 1000, // could be negative value
-    left: 0,
-    behavior: 'smooth'
-});
-
-// Scroll to a certain element
-
-function scrollLearn() {
-    document.querySelector('#learn').scrollIntoView({
-        behavior: 'smooth'
-    });
-}
-function scrollCmds() {
-    document.querySelector('#cmds').scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+$("#close").click(function() {
+    $(".modal").removeClass("is-active");
+    setTimeout(function() {},5000);
+})
